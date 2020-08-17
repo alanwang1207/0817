f25f3164597ce0bd13c2d9b1acc63a1f7fd02660
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-17 13:42:33 +0800

    建立hello.php

---
+++ hello/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method = "post" action="hello.php">
+    你的名字:
+    <input type="text" name = "txtUserName">
+    <input type="submit" name = "btnOK" value="送出">
+    </form>
+</body>
+</html>
