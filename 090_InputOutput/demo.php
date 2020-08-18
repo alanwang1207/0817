30f5d84fd62fc0c83c2928eed02a92c8f6d84ad4
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-18 11:10:16 +0800

    三元運算子

---
+++ 090_InputOutput/demo.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .fail{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+
+
+<!--
+    <ul>
+    <?php for ($i = 1; $i <= 3; $i++) {?>
+        <li><?=$i?></li>
+        <?php echo "$i" ?>
+    <?php }?>
+    </ul> -->
+
+
+
+
+<!-- <?php
+$scorelist = array(50, 100, 200, 35);
+?>
+<ul>
+<?php foreach ($scorelist as $value) {?>
+    <?php if($value>=60){ ?>
+       <li><?=$value?></li>
+    <?php } else{?>
+        <li class = "fail"><?=$value?></li>
+    <?php }?>
+<?php }?>
+</ul>
+ -->
+
+
+
+<ul>
+<?php foreach ($scorelist as $score) { ?>
+    <li class ="<?= (($score < 60) ? "fail": "") ?> ">
+        <?= $score ?>
+    </li>
+<?php }?>
+</ul>
+
+
+
+</body>
+</html>
